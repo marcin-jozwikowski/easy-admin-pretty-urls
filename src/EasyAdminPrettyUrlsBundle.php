b03079df4252ee4ecb3b2b4525711187fd029b06
Marcin Jóźwikowski <marcin@jozwikowski.pl>
2022-12-23 11:49:24 +0100

    Initial

---
+++ src/EasyAdminPrettyUrlsBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MarcinJozwikowski\EasyAdminPrettyUrls;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EasyAdminPrettyUrlsBundle extends Bundle
+{
+
+}
